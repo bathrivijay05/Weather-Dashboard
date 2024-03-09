8fdde22108f3468503049a812520edae72a25a8a
Bathrinath Vijayan <bathrivijay05@gmail.com>
2024-03-09 16:55:12 +0530

    Initial Upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# PRODIGY_WD_05
+# Weather Web Application using JavaScript
+
+This repository consists of the implementation of a weather application using JavaScript. Based on user-inputted location, it shows relevant weather information such as temperature, humidity, sunrise, sunset, wind, visibility, pressure, forecast details, etc using OpenWeatherMap API.
+
+## Features
+
+- Search Locations
+- Current Weather Details
+- 5 Days Hourly Forecast
+- Uses OpenWeatherMap API and Moment.js
+- Responsive Design
+
+## Credits
+
+- OpenWeatherMap API
+- Moment.js
+- Normalize.css
+
+## Demo Image
+
+![Weather Web Application using JavaScript](https://raw.githubusercontent.com/bathrivijay05/PRODIGY_WD_05/main/screenshot.png)
---
+++ script.js
@@ -0,0 +1,157 @@
+// Layout elements
+const forecastDetails = document.querySelector('#forecastDetails');
+const searchContainer = document.querySelector('.search-container');
+const mainTitle = document.querySelector('h1');
+const weatherContainer = document.querySelector('.weather-details');
+const searchInput = document.querySelector('.search-bar input');
+const searchButton = document.querySelector('#search-button');
+const searchBarInfo = document.querySelector('.search-container p');
+const locationList = document.querySelector('.search-results');
+
+const currentLocation = document.querySelector('#currentLocation');
+const currentTemp = document.querySelector('#currentTemp');
+const currentDesc = document.querySelector('#currentDesc');
+const sun = document.querySelector('#sun');
+const clouds = document.querySelector('#clouds');
+const wind = document.querySelector('#wind');
+const pressure = document.querySelector('#pressure');
+const humidity = document.querySelector('#humidity');
+const visibility = document.querySelector('#visibility');
+const weatherIcon = document.querySelector('#weatherIcon');
+const tempDesc = document.querySelector('#tempDesc');
+const updateTime = document.querySelector('#updateTime');
+
+// OpenWeatherMap API Key
+const apiKey = '0d501c392d1a1e67d7c5de08391f719a';
+
+// Function to search locations upon input
+async function searchLocation() {
+    const searchInput = document.querySelector('input').value;
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=9&appid=${apiKey}`);
+        const data = await response.json();
+        
+        locationList.innerHTML = '';
+
+        if (data.length == 0) {
+            searchBarInfo.innerHTML = 'No Locations found';
+            searchBarInfo.style.display = 'block';
+            locationList.style.display = 'none';
+        } else {
+            searchBarInfo.style.display = 'none';
+            locationList.style.display = 'grid';
+            data.forEach(location => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${location.name}, ${location.state}, ${location.country}`;
+                listItem.addEventListener('click', () => handleSearchInput(location.name));
+                locationList.appendChild(listItem);
+            });
+        }
+    } catch (error) {
+        console.log('Error fetching locations data:', error);
+        searchBarInfo.innerHTML = 'Location not available';
+        searchBarInfo.style.display = 'block';
+    }
+}
+
+// Function to fetch Current Weather data
+async function fetchWeatherData(location) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+// Function to fetch 5-days Forecast data
+async function fetchForecastData(location) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+// Function to control overall data fetching
+async function handleSearchInput(query) {
+    try {
+        console.log(query);
+        const data1 = await fetchWeatherData(query);
+        const data2 = await fetchForecastData(query);
+        displayWeatherDetails(data1);
+        displayForecastDetails(data2);
+    } catch (error) {
+        console.log('Error fetching weather data:', error);
+        searchBarInfo.innerHTML = 'Error in fetching weather data';
+        searchBarInfo.style.display = 'block';
+    }
+}
+
+// Display Current Weather onto webpage
+function displayWeatherDetails(weatherData) {
+
+    weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    currentLocation.innerHTML = `${weatherData.name}, ${weatherData.sys.country}`;
+    currentTemp.innerHTML = `${Math.round(weatherData.main.temp)}°C`;
+    currentDesc.innerHTML = `<p>${weatherData.weather[0].description}</p><p>Feels like ${Math.round(weatherData.main.feels_like)}°C</p>`;
+    wind.innerHTML = `${Math.round(weatherData.wind.speed * 3.6)} km/h ${degreesToDirection(weatherData.wind.deg)}`;
+    pressure.innerHTML = `${Math.round(weatherData.main.pressure)} hPa`;
+    visibility.innerHTML = `${Math.round(weatherData.visibility/1000.0)} km`;
+    humidity.innerHTML = `${Math.round(weatherData.main.humidity)}%`;
+    clouds.innerHTML = `${Math.round(weatherData.clouds.all)}%`;
+    
+    let timezone = weatherData.timezone;
+    let sunrise = weatherData.sys.sunrise;
+    let sunset = weatherData.sys.sunset;
+
+    let sunriseStr = moment.utc(sunrise,'X').add(timezone,'seconds').format('hh:mm A');
+    let sunsetStr = moment.utc(sunset,'X').add(timezone,'seconds').format('hh:mm A');
+
+    sun.innerHTML = `<span>${sunriseStr}</span><span>${sunsetStr}</span>`;
+
+    updateTime.innerHTML = moment.unix(weatherData.dt).format('hh:mm A');
+
+    tempDesc.innerHTML = `The high will be ${Math.round(weatherData.main.temp_max)}°C, the low will be ${Math.round(weatherData.main.temp_min)}°C.`;
+
+    searchContainer.style.padding = '20px 0px 40px 0px';
+    searchContainer.style.height = 'auto';
+    weatherContainer.style.display = 'block';
+    mainTitle.style.display = 'none';
+    searchBarInfo.style.display = 'none';
+}
+
+// Display 5-days weather forecast onto webpage
+function displayForecastDetails(weatherData) {
+    forecastDetails.innerHTML = '';
+
+    let timezone = weatherData.city.timezone;
+    
+    weatherData.list.forEach(day => {
+
+        let currTime = day.dt;
+        let currTimeStr = moment.utc(currTime,'X').add(timezone,'seconds').format('ddd, h A');
+        const div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = `<p>${currTimeStr}</p>
+                        <img class="item-icon" src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"></img>
+                        <p class="item-desc">${day.weather[0].description}</p>
+                        <p>${Math.round(day.main.temp_max)}° / ${Math.round(day.main.temp_min)}°</p>
+                        <p>${Math.round(day.pop * 100.0)}% Precipitation</p>`;
+        forecastDetails.appendChild(div);
+        
+    });
+}
+
+// Find Wind direction based on angle
+function degreesToDirection(degrees) {
+    const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    const index = Math.round(degrees / 45) % 8;
+    return directions[index];
+}
+
+// Event Listener
+searchButton.addEventListener('click',searchLocation);
+searchInput.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {
+        searchLocation();
+    }
+});
